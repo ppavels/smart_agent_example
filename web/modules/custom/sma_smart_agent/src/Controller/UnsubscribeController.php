1c7ff933f7093730d5cb448127352525e65a6b70
Amitai Burstein <amitai@gizra.com>
2016-07-11 23:09:54 +0300

    Fix docs

--- web/modules/custom/sma_smart_agent/src/Controller/UnsubscribeController.php
+++ web/modules/custom/sma_smart_agent/src/Controller/UnsubscribeController.php
@@ -25,20 +25,19 @@ class UnsubscribeController {
 
 
   /**
-   * Checks routing access for the node revision.
+   * Checks routing access for the smart agent unsubscribe.
    *
    * @param \Symfony\Component\Routing\Route $route
    *   The route to check against.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The currently logged in account.
-   * @param int $node_revision
-   *   (optional) The node revision ID. If not specified, but $node is, access
-   *   is checked for that object's revision.
    * @param \Drupal\node\NodeInterface $node
    *   (optional) A node object. Used for checking access to a node's default
    *   revision when $node_revision is unspecified. Ignored when $node_revision
    *   is specified. If neither $node_revision nor $node are specified, then
    *   access is denied.
+   * @param string $token
+   *   The unique token of the smart agent.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
